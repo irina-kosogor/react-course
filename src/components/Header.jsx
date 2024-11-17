f2b1c5be549c2a1a9d760037d560a2249d17d060
Irina Kosogor <i.v.kosogor@gmail.com>
2024-11-17 14:23:25 +0200

    Header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <section id="header">
+      <img
+        src="investment-calculator-logo.png"
+        alt="investmentCalculatorImage"
+      />
+      <h1>Investment Calculator</h1>
+    </section>
+  );
+}
+
+export default Header;
