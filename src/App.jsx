f45a2d3858ed99df7e397e463140b661f60b4582
Irina Kosogor <i.v.kosogor@gmail.com>
2024-11-17 15:51:21 +0200

    ResultTable Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import ResultTable from "./components/ResultTable";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
+      <ResultTable />
     </>
   );
 }
---
+++ src/components/ResultTable.jsx
@@ -0,0 +1,26 @@
+function ResultTable() {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Investment</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>$17000</td>
+          <td>$900</td>
+          <td>$900</td>
+          <td>$16200</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default ResultTable;
